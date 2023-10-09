e5f0a709798b627e0024b73be690350157bb57de
Anailson Ribeiro dos Santos <anailson.santos@corregedoria.df>
2023-10-09 12:01:02 -0300

    Projeto - API todoList

---
+++ src/main/java/br/com/anailson/todolist/controller/UserController.java
@@ -0,0 +1,17 @@
+package br.com.anailson.todolist.controller;
+
+import br.com.anailson.todolist.model.UserModel;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @PostMapping("/")
+    public void create(@RequestBody  UserModel userModel ){
+        System.out.println(userModel.getName());
+    }
+}
